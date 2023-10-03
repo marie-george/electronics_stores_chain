dd0145ac90696613a3c1fabae8b2eced120084b5
marie-george <maria85night@gmail.com>
2023-10-04 01:08:16 +0300

    added admin action for clearing debts

--- chain/admin.py
+++ chain/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from chain.models import ChainUnit
 
 
+@admin.action(description="Очистить задолженность перед поставщиком")
+def clear_debt(modeladmin, request, queryset):
+    queryset.update(debt=0)
+
+
 @admin.register(ChainUnit)
 class ChainUnitAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner', 'name', 'country', 'city', 'supplier', 'debt', 'hierarchy_level', 'creation_date')
     list_filter = ('country', 'city')
+    actions = [clear_debt]
