808e66695dbd1efb573d080c005ea878534fb3f1
marie-george <maria85night@gmail.com>
2023-10-03 23:58:12 +0300

    corrections to admin - added filter with city

--- chain/admin.py
+++ chain/admin.py
@@ -6,4 +6,4 @@ from chain.models import ChainUnit
 @admin.register(ChainUnit)
 class ChainUnitAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner', 'name', 'country', 'supplier', 'debt', 'hierarchy_level', 'creation_date')
-    list_filter = ('country',)
+    list_filter = ('country', 'city')
